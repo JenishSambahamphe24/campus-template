c17cab0257ee84b89b1ef7a44522992cb16ad7cf
JenishSambahamphe24 <limbujenish24@gmail.com>
2025-06-06 12:56:12 +0545

    ready to push

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Footer from './Components/Footer'
 import { Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import ScrollToTop from './ScrollToTop'
-import { MantineProvider } from '@mantine/core'
 
 function App() {
   return (
